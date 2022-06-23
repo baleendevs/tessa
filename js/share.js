2c11dce4f8c16527a91f5b2c46119783b9389684
Matteo Serratore <serratore.matteo@gmail.com>
2022-06-23 20:41:02 +0200

    Start to work to cie

--- js/share.js
+++ js/share.js
@@ -23,13 +23,21 @@ window.onresize = () => {
 const handleSharedCard = (card) => {
   try {
     const jsonString = atob(card);
-    const tesseraSanitaria = JSON.parse(jsonString);
-    if (!isValid(tesseraSanitaria)) {
+    const cardJSON = JSON.parse(jsonString);
+    const isTS = isValidTS(cardJSON);
+    const isCIE = isValidCIE(cardJSON);
+    if (!isTS && !isCIE) {
       return;
     }
-    setCardSize();
-    console.log(tesseraSanitaria.cF);
-    setCardFields(tesseraSanitaria);
+    if(isTS) {
+      setCardSizeTS();
+      console.log(cardJSON.cF);
+      setCardFieldsTS(cardJSON);
+    } else if(isCIE) {
+      setCardSizeCIE();
+      console.log(cardJSON.cF);
+      setCardFieldsCIE(cardJSON);
+    }
     return true;
   } catch {
     //console.log("Invalid card");
@@ -37,8 +45,11 @@ const handleSharedCard = (card) => {
   }
 };
 
-const isValid = (tesseraSanitaria) => {
+const isValidTS = (tesseraSanitaria) => {
   if (
+    tesseraSanitaria.t !== null &&
+    tesseraSanitaria.t !== undefined &&
+    tesseraSanitaria.t == "TS" &&    
     tesseraSanitaria.cF !== null &&
     tesseraSanitaria.cF !== undefined &&
     tesseraSanitaria.c !== null &&
@@ -58,6 +69,28 @@ const isValid = (tesseraSanitaria) => {
   return false;
 };
 
+const isValidCIE = (cie) => {
+  if (
+    cie.t !== null &&
+    cie.t !== undefined &&
+    cie.t == "CIE" &&        
+    cie.cF !== null &&
+    cie.cF !== undefined &&
+    cie.c !== null &&
+    cie.c !== undefined &&
+    cie.n !== null &&
+    cie.n !== undefined &&
+    cie.s !== null &&
+    cie.s !== undefined &&
+    cie.l !== null &&
+    cie.l !== undefined &&
+    cie.dN != null &&
+    cie.dN !== undefined
+  )
+    return true;
+  return false;
+};
+
 const getCardSize = (width) => {
   console.log(width);
   let height = (width * BG_IMAGE_HEIGHT) / BG_IMAGE_WIDTH;
@@ -71,7 +104,7 @@ const getCardSize = (width) => {
   };
 };
 
-const setCardSize = () => {
+const setCardSizeTS = () => {
   let tsDiv = document.querySelector(".tesseraSanitaria");
   tsDiv.style.display = "block";
   let cardSize = getCardSize(tsDiv.offsetWidth);
@@ -89,7 +122,56 @@ const setCardSize = () => {
   }
 };
 
-const setCardFields = (tesseraSanitaria) => {
+const setCardFieldsTS = (tesseraSanitaria) => {
+  let cf = document.querySelector(".tesseraSanitaria #cf");
+  cf.innerText = tesseraSanitaria.cF.toUpperCase();
+  let cognome = document.querySelector(".tesseraSanitaria #cognome");
+  cognome.innerText = tesseraSanitaria.c.toUpperCase();
+  let nome = document.querySelector(".tesseraSanitaria #nome");
+  nome.innerText = tesseraSanitaria.n.toUpperCase();
+  let sesso = document.querySelector(".tesseraSanitaria #sesso");
+  sesso.innerText = tesseraSanitaria.s.toUpperCase();
+  let provincia = document.querySelector(".tesseraSanitaria #provincia");
+  provincia.innerText = tesseraSanitaria.p.toUpperCase();
+  let luogoDiNascita = document.querySelector(
+    ".tesseraSanitaria #luogoDiNascita"
+  );
+  luogoDiNascita.innerText = tesseraSanitaria.l.toUpperCase();
+  let dataDiNascita = document.querySelector(
+    ".tesseraSanitaria #dataDiNascita"
+  );
+  dataDiNascita.innerText = tesseraSanitaria.dN.toUpperCase();
+  if (tesseraSanitaria.dS !== null && tesseraSanitaria.dS !== undefined) {
+    let dataDiScadenza = document.querySelector(
+      ".tesseraSanitaria #dataDiScadenza"
+    );
+    dataDiScadenza.innerText = tesseraSanitaria.dS.toUpperCase();
+  }
+  let tsOwner = document.querySelector("#tsOwner");
+  tsOwner.innerText = tesseraSanitaria.c + " " + tesseraSanitaria.n;
+};
+
+const setCardSizeCIE = () => {
+  let tsDiv = document.querySelector(".tesseraSanitaria");
+  tsDiv.style.display = "none";  
+  let cieDiv = document.querySelector(".cie");
+  cieDiv.style.display = "block";
+  let cardSize = getCardSize(cieDiv.offsetWidth);
+  console.log(cardSize);
+  cieDiv.style.height = cardSize.height + "px";
+  let tsImg = document.querySelector(".cie > img");
+  cieDiv.style.borderRadius = cardSize.borderRadius + "px";
+  tsImg.style.borderRadius = cardSize.borderRadius + "px";
+  let cardFields = document.querySelectorAll(".tesseraSanitaria > span");
+  for (const key in cardFields) {
+    if (cardFields.hasOwnProperty(key)) {
+      const field = cardFields[key];
+      field.style.fontSize = cardSize.fontSize + "px";
+    }
+  }
+};
+
+const setCardFieldsCIE = (tesseraSanitaria) => {
   let cf = document.querySelector(".tesseraSanitaria #cf");
   cf.innerText = tesseraSanitaria.cF.toUpperCase();
   let cognome = document.querySelector(".tesseraSanitaria #cognome");
