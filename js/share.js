3806470069dabc02e96434dcc2969c3c9ee6cdaa
Carlo Andreotti <c.andreotti@m3s.it>
2022-06-24 12:42:44 +0200

    Refactor js code and complete CIE fields positioning

--- js/share.js
+++ js/share.js
@@ -14,213 +14,160 @@ window.onload = () => {
 };
 
 window.onresize = () => {
-  let tsDiv = document.querySelector(".tesseraSanitaria");
-  if (tsDiv.style.display !== "none") {
-    setCardSize();
+  let cardDivs = document.querySelectorAll(".tessaCard");
+  for (const key in cardDivs) {
+    if (Object.hasOwnProperty.call(cardDivs, key)) {
+      try {
+        const cardDiv = cardDivs[key];
+        const display = window.getComputedStyle(cardDiv).display;
+        if (display !== "none") {
+          if (cardDiv.classList.contains("ts")) {
+            setCardSize("ts");
+          } else if (cardDiv.classList.contains("cie")) {
+            setCardSize("cie");
+          }
+        }
+      } catch {}
+    }
   }
 };
 
 const handleSharedCard = (card) => {
   try {
     const jsonString = atob(card);
-    const cardJSON = JSON.parse(jsonString);
-    const isTS = isValidTS(cardJSON);
-    const isCIE = isValidCIE(cardJSON);
-    if (!isTS && !isCIE) {
-      return;
-    }
-    if(isTS) {
-      setCardSizeTS();
-      console.log(cardJSON.cF);
-      setCardFieldsTS(cardJSON);
-    } else if(isCIE) {
-      setCardSizeCIE();
-      console.log(cardJSON.cF);
-      setCardFieldsCIE(cardJSON);
-    }
+    const parsedCard = JSON.parse(jsonString);
+    parsedCard.t = parsedCard.t == null ? "ts" : parsedCard.t.toLowerCase();
+    if (!isValid(parsedCard)) return false;
+    setCardSize(parsedCard.t);
+    console.log(parsedCard.cF);
+    setCardFields(parsedCard);
     return true;
-  } catch {
-    //console.log("Invalid card");
+  } catch (e) {
+    console.log("Invalid card", e);
     return false;
   }
 };
 
-const isValidTS = (tesseraSanitaria) => {
-  if (
-    tesseraSanitaria.t !== null &&
-    tesseraSanitaria.t !== undefined &&
-    tesseraSanitaria.t == "TS" &&    
-    tesseraSanitaria.cF !== null &&
-    tesseraSanitaria.cF !== undefined &&
-    tesseraSanitaria.c !== null &&
-    tesseraSanitaria.c !== undefined &&
-    tesseraSanitaria.n !== null &&
-    tesseraSanitaria.n !== undefined &&
-    tesseraSanitaria.s !== null &&
-    tesseraSanitaria.s !== undefined &&
-    tesseraSanitaria.p !== null &&
-    tesseraSanitaria.p !== undefined &&
-    tesseraSanitaria.l !== null &&
-    tesseraSanitaria.l !== undefined &&
-    tesseraSanitaria.dN != null &&
-    tesseraSanitaria.dN !== undefined
-  )
-    return true;
+const isValid = (card) => {
+  switch (card.t) {
+    case "ts":
+      if (
+        card.t !== null &&
+        card.t !== undefined &&
+        card.cF !== null &&
+        card.cF !== undefined &&
+        card.c !== null &&
+        card.c !== undefined &&
+        card.n !== null &&
+        card.n !== undefined &&
+        card.s !== null &&
+        card.s !== undefined &&
+        card.p !== null &&
+        card.p !== undefined &&
+        card.l !== null &&
+        card.l !== undefined &&
+        card.dN != null &&
+        card.dN !== undefined
+      )
+        return true;
+      break;
+    case "cie":
+      if (
+        card.t !== null &&
+        card.t !== undefined &&
+        card.cF !== null &&
+        card.cF !== undefined &&
+        card.c !== null &&
+        card.c !== undefined &&
+        card.n !== null &&
+        card.n !== undefined &&
+        card.s !== null &&
+        card.s !== undefined &&
+        ((card.lN !== null &&
+          card.lN !== undefined) ||
+          (card.l !== null &&
+          card.l !== undefined)) &&
+        card.p !== null &&
+        card.p !== undefined &&    
+        card.dN != null &&
+        card.dN !== undefined
+      )
+        return true;
+      break;
+    default:
+      break;
+  }
   return false;
-};
+}
 
-const isValidCIE = (cie) => {
-  if (
-    cie.t !== null &&
-    cie.t !== undefined &&
-    cie.t == "CIE" &&        
-    cie.cF !== null &&
-    cie.cF !== undefined &&
-    cie.c !== null &&
-    cie.c !== undefined &&
-    cie.n !== null &&
-    cie.n !== undefined &&
-    cie.s !== null &&
-    cie.s !== undefined &&
-    cie.lN !== null &&
-    cie.lN !== undefined &&
-    cie.p !== null &&
-    cie.p !== undefined &&    
-    cie.dN != null &&
-    cie.dN !== undefined
-  )
-    return true;
-  return false;
-};
-
-const getCardSize = (width) => {
-  console.log(width);
+const getCardSize = (width, cardType) => {
+  //console.log(width);
   let height = (width * BG_IMAGE_HEIGHT) / BG_IMAGE_WIDTH;
   let borderRadius = (width * 50) / BG_IMAGE_WIDTH;
   let fontSize = (90 * height) / BG_IMAGE_HEIGHT;
+  if (cardType == "cie") {
+    fontSize = (70 * height) / BG_IMAGE_HEIGHT;
+  }
+  let fontSizeNs = (94 * height) / BG_IMAGE_HEIGHT;
+  let fontSizeCAN = (115 * height) / BG_IMAGE_HEIGHT;
   return {
     width: width,
     height: height,
     borderRadius: borderRadius,
     fontSize: fontSize,
+    fontSizeNs: fontSizeNs,
+    fontSizeCAN: fontSizeCAN
   };
 };
 
-const setCardSizeTS = () => {
-  let tsDiv = document.querySelector(".tesseraSanitaria");
-  tsDiv.style.display = "block";
-  let cardSize = getCardSize(tsDiv.offsetWidth);
-  console.log(cardSize);
-  tsDiv.style.height = cardSize.height + "px";
-  let tsImg = document.querySelector(".tesseraSanitaria > img");
-  tsDiv.style.borderRadius = cardSize.borderRadius + "px";
-  tsImg.style.borderRadius = cardSize.borderRadius + "px";
-  let cardFields = document.querySelectorAll(".tesseraSanitaria > span");
+const setCardSize = (cardType) => {
+  let cardDiv = document.querySelector(`.${cardType}`);
+  cardDiv.style.display = "block";
+  let cardSize = getCardSize(cardDiv.offsetWidth, cardType);
+  //console.log(cardSize);
+  cardDiv.style.height = cardSize.height + "px";
+  let cardImg = document.querySelector(`.${cardType} > img`);
+  cardDiv.style.borderRadius = cardSize.borderRadius + "px";
+  cardImg.style.borderRadius = cardSize.borderRadius + "px";
+  let cardFields = document.querySelectorAll(`.${cardType} > span`);
   for (const key in cardFields) {
     if (cardFields.hasOwnProperty(key)) {
       const field = cardFields[key];
       field.style.fontSize = cardSize.fontSize + "px";
+      if (cardType === "cie") {
+        if (field.classList.contains("nS")) {
+          field.style.fontSize = cardSize.fontSizeNs + "px";
+        } else if (field.classList.contains("cAN")) {
+          field.style.fontSize = cardSize.fontSizeCAN + "px";
+        }
+      }
     }
   }
-};
+  let cardMessage = document.querySelector(`.content-title #${cardType}Message`);
+  cardMessage.style.display = "block";
+}
 
-const setCardFieldsTS = (tesseraSanitaria) => {
-  let cf = document.querySelector(".tesseraSanitaria #cf");
-  cf.innerText = tesseraSanitaria.cF.toUpperCase();
-  let cognome = document.querySelector(".tesseraSanitaria #cognome");
-  cognome.innerText = tesseraSanitaria.c.toUpperCase();
-  let nome = document.querySelector(".tesseraSanitaria #nome");
-  nome.innerText = tesseraSanitaria.n.toUpperCase();
-  let sesso = document.querySelector(".tesseraSanitaria #sesso");
-  sesso.innerText = tesseraSanitaria.s.toUpperCase();
-  let provincia = document.querySelector(".tesseraSanitaria #provincia");
-  provincia.innerText = tesseraSanitaria.p.toUpperCase();
-  let luogoDiNascita = document.querySelector(
-    ".tesseraSanitaria #luogoDiNascita"
-  );
-  luogoDiNascita.innerText = tesseraSanitaria.l.toUpperCase();
-  let dataDiNascita = document.querySelector(
-    ".tesseraSanitaria #dataDiNascita"
-  );
-  dataDiNascita.innerText = tesseraSanitaria.dN.toUpperCase();
-  if (tesseraSanitaria.dS !== null && tesseraSanitaria.dS !== undefined) {
-    let dataDiScadenza = document.querySelector(
-      ".tesseraSanitaria #dataDiScadenza"
-    );
-    dataDiScadenza.innerText = tesseraSanitaria.dS.toUpperCase();
-  } 
-  let tsOwner = document.querySelector("#tsOwner");
-  tsOwner.innerText = tesseraSanitaria.c + " " + tesseraSanitaria.n;
-};
-
-const setCardSizeCIE = () => {
-  let tsDiv = document.querySelector(".tesseraSanitaria");
-  tsDiv.style.display = "none";  
-  let cieDiv = document.querySelector(".cie");
-  cieDiv.style.display = "block";
-  let cardSize = getCardSize(cieDiv.offsetWidth);
-  console.log(cardSize);
-  cieDiv.style.height = cardSize.height + "px";
-  let cieImg = document.querySelector(".cie > img");
-  cieDiv.style.borderRadius = cardSize.borderRadius + "px";
-  cieImg.style.borderRadius = cardSize.borderRadius + "px";
-  let cardFields = document.querySelectorAll(".cie > span");
-  for (const key in cardFields) {
-    if (cardFields.hasOwnProperty(key)) {
-      const field = cardFields[key];
-      field.style.fontSize = cardSize.fontSize + "px";
+const setCardFields = (card) => {
+  if (card.t === "cie" && card.s != null && card.s.toUpperCase() === "F") {
+    let cardBg = document.querySelector(`.${card.t} img`);
+    cardBg.src = cardBg.src.replace("male", "female");
+  }
+  let cardFields = Object.entries(card);
+  for (const i in cardFields) {
+    if (Object.hasOwnProperty.call(cardFields, i)) {
+      const key = cardFields[i][0];
+      let value = cardFields[i][1];
+      if (typeof value !== "string") {
+        value = value.toString();
+      }
+      let cardFieldDiv = document.querySelector(`.${card.t} .${key}`);
+      if (cardFieldDiv != null) {
+        cardFieldDiv.innerText = value == null ? "" : value.toUpperCase();
+      }
     }
   }
-
-  let tsDesc = document.querySelector(".content-title #tsDesc");
-  tsDesc.style.display = "none";  
-  let cieDesc = document.querySelector(".content-title #cieDesc");
-  cieDesc.style.display = "block";  
-};
-
-const setCardFieldsCIE = (cie) => {
-  let cf = document.querySelector(".cie #cf");
-  cf.innerText = cie.cF.toUpperCase();
-  let cognome = document.querySelector(".cie #cognome");
-  cognome.innerText = cie.c.toUpperCase();
-  let nome = document.querySelector(".cie #nome");
-  nome.innerText = cie.n.toUpperCase();
-  let sesso = document.querySelector(".cie #sesso");
-  sesso.innerText = cie.s.toUpperCase();
-  if(cie.s.toUpperCase() == "F") {
-    let img = document.querySelector(".cie #img");
-    img.src = img.src.replace("male", "female");
+  let cardOwner = document.querySelector(`#${card.t}Owner`);
+  if (cardOwner != null) {
+    cardOwner.innerText = `${card.c} ${card.n}`;
   }
-  if (cie.st !== null && cie.st !== undefined) {
-    let statura = document.querySelector(".cie #statura");
-    statura.innerText = cie.st.toUpperCase();
-  }               
-  let luogoEDataDiNascita = document.querySelector(".cie #luogoEDataDiNascita");
-  luogoEDataDiNascita.innerText = cie.lN.toUpperCase() + " (" + cie.lN.toUpperCase() + ") " + cie.dN.toUpperCase();
-  if (cie.dE !== null && cie.dE !== undefined) {
-    let dataDiEmissione = document.querySelector(
-      ".cie #dataDiEmissione"
-    );
-    dataDiEmissione.innerText = cie.dS.toUpperCase();
-  }
-  if (cie.nz !== null && cie.nz !== undefined) {
-    let nazionalita = document.querySelector(
-      ".cie #nazionalita"
-    );
-    nazionalita.innerText = cie.nz.toUpperCase();   
-  }  
-  if (cie.cAN !== null && cie.cAN !== undefined) {
-    let cardAccessNumber = document.querySelector(
-      ".cie #cardAccessNumber"
-    );
-    cardAccessNumber.innerText = cie.cAN.toUpperCase();  
-  }  
-  if (cie.dS !== null && cie.dS !== undefined) {
-    let dataDiScadenza = document.querySelector(
-      ".cie #dataDiScadenza"
-    );
-    dataDiScadenza.innerText = cie.dS.toUpperCase();
-  }
-  let cieOwner = document.querySelector("#cieOwner");
-  cieOwner.innerText = cie.c + " " + cie.n;
-};
+}
